5cac0d9c9b1ffcd96e7202c764c7be8beb8fed6c
ZiedDev <73042811+ZiedDev@users.noreply.github.com>
2023-08-06 01:45:07 +0300

    v0.1 I got something working

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get Planets button (useless till now)
+const mercury = document.getElementById("mercury");
+const venus = document.getElementById("venus");
+const earth = document.getElementById("earth");
+const mars = document.getElementById("mars");
+const jupiter = document.getElementById("jupiter");
+const saturn = document.getElementById("saturn");
+const uranus = document.getElementById("uranus");
+const neptune = document.getElementById("neptune");
+const pluto = document.getElementById("pluto");
+
+// Get UI Elements
+const contentHeader = document.getElementById("content-header");
+const content = document.getElementById("content");
+const contentSource = document.getElementById("content-source");
+const infoCards = document.getElementById("info-cards");
+const infoCard1 = document.getElementById("info-card1");
+const infoCard2 = document.getElementById("info-card2");
+const infoCard3 = document.getElementById("info-card3");
+const infoCard4 = document.getElementById("info-card4");
